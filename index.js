8515092e260546719a162cf08892756678113786
Nency Modi <nencymodi150@gmail.com>
2024-12-19 19:41:14 -0500

    main

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");   
+
+
+app.listen(PORT, ()=>{ console.log("go to http http://localhost:3000/")
+
+})
